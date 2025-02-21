0d4ba78141995b721e23307667cb85ed8f21e592
marshot9420 <124557537+marshot9420@users.noreply.github.com>
2025-02-21 12:24:24 +0900

    configs: @nestjs/config + .env 파일 적용

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,39 @@
+import { INestApplication, Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+class Application {
+  private logger = new Logger(Application.name);
+  private HOST: string;
+  private PORT: string;
+
+  constructor(private app: INestApplication) {
+    this.app = app;
+    this.HOST = process.env.HOST;
+    this.PORT = process.env.PORT;
+  }
+
+  async bootstrap() {
+    this.app = await NestFactory.create(AppModule);
+    await this.app.listen(this.PORT);
+  }
+
+  startLog() {
+    this.logger.log(`✅ 서버가 ${this.HOST}:${this.PORT} 에서 실행 중입니다🚀`);
+  }
 }
-bootstrap();
+
+async function init() {
+  const server = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+
+  const app = new Application(server);
+
+  await app.bootstrap();
+
+  app.startLog();
+}
+
+init();
